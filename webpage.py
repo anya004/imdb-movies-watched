2025e7d2ff6a4a0b997fc27af0a7900a8df4fc0f
anya004 <annavakulyuk@gmail.com>
2016-08-26 12:36:57 -0400

    imported IMDB library; wrote SearchResultsFunction

--- webpage.py
+++ webpage.py
@@ -3,6 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import func
 from imdb_setup import Base, Movie, Person, Roles, User, MoviesWatched
+from imdbpie import Imdb
+
+imdb = Imdb()
+imdb = Imdb(anonymize=True) # to proxy requests
+
+# Creating an instance with caching enabled
+# Note that the cached responses expire every 2 hours or so.
+# The API response itself dictates the expiry time)
+imdb = Imdb(cache=True)
+
 app = Flask(__name__)
 
 #Create session and connect to the database
@@ -21,9 +31,22 @@ session.commit()
 @app.route('/home/', methods=['GET', 'POST'])
 def showHomePage():
     if request.method == 'POST':
+        search_for = request.form['title']
+        return redirect(url_for('showSearchResults', search_for = search_for))
     else:
         return render_template('homepage.html')
 
+@app.route('/search/<string:search_for>/')
+def showSearchResults(search_for):
+    results = imdb.search_for_title(search_for)
+    image_urls = {}
+    for r in results:
+        image_url = imdb.get_title_by_id(r['imdb_id'])
+        if not image_url.poster_url:
+            image_urls[r['imdb_id']] = "http://ia.media-imdb.com/images/G/01/imdb/images/nopicture/32x44/film-3119741174._CB282925985_.png"
+        else:
+            image_urls[r['imdb_id']] = image_url.poster_url
+    return render_template('search_results.html', results=results, image_urls=image_urls)
 if __name__ == '__main__':
     #app.secret_key = "super_secret_key"
     app.debug = True
